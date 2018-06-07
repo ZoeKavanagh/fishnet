db62da922abafb03ba7eabeb00e5dceabfacf6da
benfurber <ben@furber-consultancy.com>
2018-06-07 14:15:07 +0100

    Debugged some issues we got when deploying locally

--- spec/features/user_can_view_posts_spec.rb
+++ spec/features/user_can_view_posts_spec.rb
@@ -7,5 +7,8 @@ RSpec.feature 'View post', type: :feature do
   scenario 'User can view a post' do
     visit '/posts'
     expect(page).to have_css('.post')
+    expect(page).to have_css('.post-user')
+    expect(page).to have_css('.post-body')
+    expect(page).to have_css('.post-timestamp')
   end
 end
