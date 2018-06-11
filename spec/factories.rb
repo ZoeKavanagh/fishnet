3a3bf6f07ff198dbeed8de7805f40eefc082dd76
Dione Estabillo <dione.estabillo@gmail.com>
2018-06-11 12:12:22 +0100

    Added User feature tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
       body 'Sandroller horn shark walu. Candlefish mosquitofish morwong spiny eel kahawai Dolly Varden trout emperor angelfish eagle ray priapumfish paradise fish driftfish'
     end
   end
+
+  factory :user do
+    email 'salmon@ocean.com'
+    firstname 'Codin'
+    lastname 'Octosmith'
+    password 'flipflipflip'
+    password_confirmation 'flipflipflip'
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before (:each) do
+    @user = create(:user)
+  end
+
+  context '#create' do
+    it 'adds an email address' do
+      expect(User.all.last.email).to eq @user.email
+    end
+
+    it 'adds a first name' do
+      expect(User.all.last.firstname).to eq @user.firstname
+    end
+
+    it 'adds a last name' do
+      expect(User.all.last.lastname).to eq @user.lastname
+    end
+  end
 end
