f910fca44e223d76759ae0b4345427b6b4baf701
Mark Channer <markchanner@gmail.com>
2018-06-11 11:43:43 +0100

    Adds 'before_action :authenticate_user!' to application_controller.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_07_140204) do
+ActiveRecord::Schema.define(version: 2018_06_08_103908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
