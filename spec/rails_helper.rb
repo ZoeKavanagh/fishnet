94f5bb70c65c8a27626cfdf4d772ac3bb3be3808
benfurber <ben@furber-consultancy.com>
2018-06-14 16:51:32 +0100

    Added coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,13 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.start 'rails'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'database_cleaner'
 require 'support/factory_bot'
 require 'support/login_helper'
@@ -13,9 +18,13 @@ DatabaseCleaner.strategy = :truncation
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  SimpleCov::Formatter::HTMLFormatter
+])
 
 RSpec.configure do |config|
 
